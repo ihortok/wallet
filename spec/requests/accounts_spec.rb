c740f5c5eb06968dd28ee101f7a95768a69defe0
ihortok <itokaryk@gmail.com>
2020-11-10 20:27:03 +0200

    account spec refactor

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,29 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe '/accounts', type: :request do
-  let(:valid_attributes) {
-    skip('Add a hash of attributes valid for your model')
-  }
+  let(:account_ownership) { create(:account_ownership) }
+  let(:user) { account_ownership.user }
+  let(:account) { account_ownership.account }
 
-  let(:invalid_attributes) {
-    skip('Add a hash of attributes invalid for your model')
-  }
-
-  before { sign_in create(:user) }
+  before { sign_in user }
 
   describe 'GET /index' do
     it 'renders a successful response' do
-      Account.create! valid_attributes
       get accounts_url
       expect(response).to be_successful
+      expect(response.body).to include(account.name)
     end
   end
 
   describe 'GET /show' do
     it 'renders a successful response' do
-      account = Account.create! valid_attributes
       get account_url(account)
       expect(response).to be_successful
+      expect(response.body).to include(account.name)
     end
   end
 
@@ -36,16 +32,31 @@ RSpec.describe '/accounts', type: :request do
 
   describe 'GET /edit' do
     it 'render a successful response' do
-      account = Account.create! valid_attributes
       get edit_account_url(account)
       expect(response).to be_successful
     end
   end
 
   describe 'POST /create' do
+    let(:valid_attributes) do
+      {
+        name: Faker::Lorem.word,
+        note: Faker::Lorem.sentence,
+        balance: Faker::Number.number(digits: 4)
+      }
+    end
+    let(:invalid_attributes) do
+      {
+        title: Faker::Lorem.word,
+        user: user
+      }
+    end
+
     context 'with valid parameters' do
       it 'creates a new Account' do
-        expect { post accounts_url, params: { account: valid_attributes } }.to change(Account, :count).by(1)
+        expect do
+          post accounts_url, params: { account: valid_attributes }
+        end.to change(Account, :count).by(1)
       end
 
       it 'redirects to the created account' do
@@ -56,9 +67,9 @@ RSpec.describe '/accounts', type: :request do
 
     context 'with invalid parameters' do
       it 'does not create a new Account' do
-        expect {
+        expect do
           post accounts_url, params: { account: invalid_attributes }
-        }.to change(Account, :count).by(0)
+        end.to change(Account, :count).by(0)
       end
 
       it "renders a successful response (i.e. to display the 'new' template)" do
@@ -69,43 +80,33 @@ RSpec.describe '/accounts', type: :request do
   end
 
   describe 'PATCH /update' do
-    context 'with valid parameters' do
-      let(:new_attributes) {
-        skip('Add a hash of attributes valid for your model')
+    let(:new_attributes) do
+      {
+        name: Faker::Lorem.word,
+        note: Faker::Lorem.sentence,
+        balance: Faker::Number.number(digits: 4)
       }
-
-      it 'updates the requested account' do
-        account = Account.create! valid_attributes
-        patch account_url(account), params: { account: new_attributes }
-        account.reload
-        skip('Add assertions for updated state')
-      end
-
-      it 'redirects to the account' do
-        account = Account.create! valid_attributes
-        patch account_url(account), params: { account: new_attributes }
-        account.reload
-        expect(response).to redirect_to(account_url(account))
-      end
     end
 
-    context 'with invalid parameters' do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        account = Account.create! valid_attributes
-        patch account_url(account), params: { account: invalid_attributes }
-        expect(response).to be_successful
-      end
+    it 'updates the requested account' do
+      patch account_url(account), params: { account: new_attributes }
+      account.reload
+      expect(account.name).to eq(new_attributes[:name])
+    end
+
+    it 'redirects to the account' do
+      patch account_url(account), params: { account: new_attributes }
+      account.reload
+      expect(response).to redirect_to(account_url(account))
     end
   end
 
   describe 'DELETE /destroy' do
     it 'destroys the requested account' do
-      account = Account.create! valid_attributes
       expect { delete account_url(account) }.to change(Account, :count).by(-1)
     end
 
     it 'redirects to the accounts list' do
-      account = Account.create! valid_attributes
       delete account_url(account)
       expect(response).to redirect_to(accounts_url)
     end
