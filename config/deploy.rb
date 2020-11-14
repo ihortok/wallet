d5ce74e7f79bb72f71f5ac235ca555f8b510465d
ihortok <itokaryk@gmail.com>
2020-11-15 00:45:16 +0200

    deploy (#6)
    * capistrano
    
    * cap install
    
    * cap settings
    
    * secret.yml
    
    * passenger_restart_with_touch

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+set :application, "wallet"
+set :repo_url, "git@github.com:ihortok/wallet.git"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
+# Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
+# This is useful if you don't want to use ENV variables
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+set :branch, 'deploy'
+set :passenger_restart_with_touch, true
