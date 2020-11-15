926f8faadfbbcc7933337d431fad17364a984c6d
ihortok <itokaryk@gmail.com>
2020-11-15 16:31:38 +0200

    currencies table

---
+++ db/migrate/20201115132337_create_currencies.rb
@@ -0,0 +1,12 @@
+class CreateCurrencies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :currencies do |t|
+      t.string :country
+      t.string :code
+      t.string :symbol
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_170914) do
+ActiveRecord::Schema.define(version: 2020_11_15_132337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2020_10_09_170914) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "currency_cd"
+  end
+
+  create_table "currencies", force: :cascade do |t|
+    t.string "country"
+    t.string "code"
+    t.string "symbol"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "operations", force: :cascade do |t|
