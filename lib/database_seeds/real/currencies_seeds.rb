a3445ae5bada4ab0f440d9a6def1bd6cc39da90b
ihortok <itokaryk@gmail.com>
2020-11-19 23:06:18 +0200

    currency rate

--- lib/database_seeds/real/currencies_seeds.rb
+++ lib/database_seeds/real/currencies_seeds.rb
@@ -10,6 +10,7 @@ module DatabaseSeeds
           new_currency.country = currency[:country]
           new_currency.symbol = currency[:symbol]
           new_currency.icon = currency[:icon]
+          new_currency.rate = currency[:rate]
           new_currency.save
         end
       end
@@ -18,13 +19,13 @@ module DatabaseSeeds
 
       def currencies
         [
-          { id: 1, country: 'Ukraine', code: 'UAH', symbol: '₴', icon: 'ukraine' },
-          { id: 2, country: 'United States', code: 'USD', symbol: '$', icon: 'us' },
-          { id: 3, country: 'European Union', code: 'EUR', symbol: '€', icon: 'eu' },
-          { id: 4, country: 'United Kindom', code: 'GBP', symbol: '£', icon: 'uk' },
-          { id: 5, country: 'Poland', code: 'PLN', symbol: 'zł', icon: 'poland' },
-          { id: 6, country: 'Thailand', code: 'THB', symbol: '฿', icon: 'thailand' },
-          { id: 7, country: 'Hungary', code: 'HUF', symbol: 'Ft', icon: 'hungary' }
+          { id: 1, country: 'Ukraine', code: 'UAH', symbol: '₴', icon: 'ukraine', rate: 28.35 },
+          { id: 2, country: 'United States', code: 'USD', symbol: '$', icon: 'us', rate: 1.0 },
+          { id: 3, country: 'European Union', code: 'EUR', symbol: '€', icon: 'eu', rate: 0.84 },
+          { id: 4, country: 'United Kindom', code: 'GBP', symbol: '£', icon: 'uk', rate: 0.75 },
+          { id: 5, country: 'Poland', code: 'PLN', symbol: 'zł', icon: 'poland', rate: 3.76 },
+          { id: 6, country: 'Thailand', code: 'THB', symbol: '฿', icon: 'thailand', rate: 30.37 },
+          { id: 7, country: 'Hungary', code: 'HUF', symbol: 'Ft', icon: 'hungary', rate: 301.6 }
         ]
       end
     end
