b471640f41669c582d1a6552b255db3e37781b26
ihortok <itokaryk@gmail.com>
2020-12-19 19:14:57 +0200

    base

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome_page#index'
   get '/about', to: 'about_page#index'
+  get '/profile', to: 'profiles#show'
   resources :accounts
   resources :operations
 end
