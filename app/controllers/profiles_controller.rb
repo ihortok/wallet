d1f4d6366d9be7f3e121f4f18882c3ec77ee5a61
ihortok <itokaryk@gmail.com>
2021-01-02 23:39:21 +0200

    show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,12 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile, only: %i[show]
 
-  def show
-    @user = current_user
+  def show; end
+
+  private
+
+  def set_profile
     @profile = current_user.profile
   end
 end
