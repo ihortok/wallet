0c60fced7b74cb9efcdbe4ae3174237a39304c06
ihortok <itokaryk@gmail.com>
2021-01-03 16:33:47 +0200

    profile show & edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,26 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_profile, only: %i[show]
+  before_action :set_profile, only: %i[show edit update]
 
   def show; end
+
+  def edit; end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to @profile, notice: 'Account was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
   private
 
   def set_profile
     @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :date_of_birth)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'welcome_page#index'
   get '/about', to: 'about_page#index'
   get '/profile', to: 'profiles#show'
+  get '/profile/edit', to: 'profiles#edit'
+  post '/profile/update', to: 'profiles#update'
   resources :accounts
   resources :operations
 end
