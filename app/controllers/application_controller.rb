66e5e761a049bdd4c78ecf325bd64774d45705b2
ihortok <itokaryk@gmail.com>
2021-06-06 23:26:20 +0300

    hotfix: fetch_accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 # ApplicationController
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :fetch_accounts
+  before_action :fetch_accounts, if: :user_signed_in?
 
   private
 
