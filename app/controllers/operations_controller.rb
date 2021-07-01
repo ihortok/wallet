23df50eb6a6e1dc064dfc5ec67305a1e3c14521d
ihortok <itokaryk@gmail.com>
2021-07-01 23:40:36 +0300

    operations refactor

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
+# OperationsController
 class OperationsController < ApplicationController
   before_action :set_operation, only: %i[show edit update destroy]
-  before_action :set_operation_params, only: :create
 
   def index
     @operations = current_user.operations
@@ -15,21 +17,12 @@ class OperationsController < ApplicationController
   def create
     @operation = Operation.new(operation_params.merge({ user_id: current_user.id }))
 
-    if @operation.save
-      case @type_cd
-      when 0 then @debit_account.update(balance: @debit_account.balance + @sum)
-      when 1 then @credit_account.update(balance: @credit_account.balance - @sum)
-      when 2
-        if @credit_account == @debit_account
-          @credit_account.update(balance: @credit_account.balance - @fee)
-        else
-          currecies_check
-          @credit_account.update(balance: @credit_account.balance - (@sum + @fee))
-          @debit_account.update(balance: @debit_account.balance + (@converted_sum || @sum))
-        end
-      end
+    if @operation.valid?
+      result = AppServices::CreateOperationAndModifyAccounts.new(@operation).call
 
-      redirect_to @operation, notice: 'Operation was successfully created.'
+      redirect_to @operation, notice: 'Operation was successfully created.' and return if result.success?
+
+      redirect_to operations_path, alert: result.error
     else
       render :new
     end
@@ -37,9 +30,8 @@ class OperationsController < ApplicationController
 
   def destroy
     @operation.destroy
-    respond_to do |format|
-      format.html { redirect_to operations_url, notice: 'Operation was successfully destroyed.' }
-    end
+
+    redirect_to operations_url, notice: 'Operation was successfully destroyed.'
   end
 
   private
@@ -50,24 +42,5 @@ class OperationsController < ApplicationController
 
   def operation_params
     params.require(:operation).permit(:type_cd, :debit_account_id, :credit_account_id, :sum, :fee, :note)
-  end
-
-  def set_operation_params
-    @type_cd = operation_params[:type_cd].to_i
-    @debit_account = Account.find_by(id: operation_params[:debit_account_id])
-    @credit_account = Account.find_by(id: operation_params[:credit_account_id])
-    @sum = operation_params[:sum].to_i
-    @fee = operation_params[:fee].to_i
-  end
-
-  def currecies_check
-    credit_account_rate = @credit_account.currency&.rate
-    debit_account_rate = @debit_account.currency&.rate
-
-    return unless credit_account_rate && debit_account_rate
-
-    return if credit_account_rate == debit_account_rate
-
-    @converted_sum = (@sum / credit_account_rate * debit_account_rate).to_i
   end
 end
