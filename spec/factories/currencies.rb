ce46cc9c65114dfbe25534ed07a8d0c67722e260
ihortok <itokaryk@gmail.com>
2021-07-02 23:49:14 +0300

    different currencies spec (#33)

--- spec/factories/currencies.rb
+++ spec/factories/currencies.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :currency do
     country { Faker::Number.number(digits: 4) }
     code { Faker::Currency.code }
     symbol { Faker::Address.country }
+    icon { :eu }
+    rate { 1.0 }
   end
 end
